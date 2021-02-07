42ea979f2e24af406bd29957a3717c218817469c
Stephen Flores <sflores813@gmail.com>
2021-02-06 23:11:01 -0600

    edited Navbar, added hero section

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,31 +1,45 @@
-import React from 'react'
-import './Button.css'
+// import React from 'react';
+// import './Button.css';
+// import { Link } from 'react-router-dom';
 
-const STYLES = [
-    'btn--primary',
-    'btn--outline'
-]
+// export function Button() {
+//   return (
+//     <Link to='sign-up'>
+//       <button className='btn'>Sign Up</button>
+//     </Link>
+//   );
+// }
 
-const SIZES = [
-    'btn--medium',
-    'btn--large'
-]
+import React from 'react';
+import './Button.css';
+import { Link } from 'react-router-dom';
+
+const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
+
+const SIZES = ['btn--medium', 'btn--large'];
 
 export const Button = ({
-    children,
-    type,
-    onClick,
-    buttonStyle,
-    buttonSize
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize
 }) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
 
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
-
-    return (
-        <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
-            {children}
-        </button>
-    )
-}
+  return (
+    <Link to='/contact' className='btn-mobile'>
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};
